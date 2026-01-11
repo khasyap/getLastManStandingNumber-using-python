97a965dd7f9e8830822a80976e78c041c346e481
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-01-11 13:14:08 +0530

    Create getLastManStandingNumber.py

---
+++ getLastManStandingNumber.py
@@ -0,0 +1,12 @@
+def getLastManStandingNumber(n, k):
+    survivor = 0
+    for i in range(1, n + 1):
+        survivor = (survivor + k) % i
+    return survivor + 1
+
+
+# Taking input from user
+n, k = map(int, input().split())
+
+# Printing output
+print(getLastManStandingNumber(n, k))
